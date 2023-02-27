c5444fb5a05bd1e2bf413aa9a0c9463d735b22ec
Conjac76 <125162476+Conjac76@users.noreply.github.com>
2023-02-27 13:00:32 -0800

    Add files via upload

---
+++ countContract.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.4;
+
+contract CountContract {
+  uint public count;
+
+  constructor (uint _count) {
+    count = _count;
+  }
+
+  function setCount (uint _count) public {
+    count = _count;
+  }
+
+  function getCount () public view returns(uint) {
+      return count;
+  }
+
+  function increment() public {
+    count++;
+  }
+
+  function decrement() public {
+    count--;
+  }
+}
